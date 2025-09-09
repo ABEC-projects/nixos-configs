292b02852a2421c39417f1e75cc179b5bb6f5fa7
ABEC-projects <70073993+ABEC-projects@users.noreply.github.com>
2025-09-09 20:38:27 +0300

    1

--- plug/makeModule.nix
+++ plug/makeModule.nix
@@ -7,20 +7,20 @@
 }:
 {
   imports = [
-    ./default.nix
-      home-manager.nixosModules.home-manager
-      stylix.nixosModules.stylix
-      copyparty.nixosModules.default
-      niri.nixosModules.niri
-      {
-        environment.systemPackages = [
-          nix-alien.packages.x86_64-linux.nix-alien
-        ];
-        nixpkgs.overlays = [
-          copyparty.overlays.default
-            niri.overlays.niri
-        ];
-        programs.nix-ld.enable = true;
-      }
+    ./plug.nix
+    home-manager.nixosModules.home-manager
+    stylix.nixosModules.stylix
+    copyparty.nixosModules.default
+    niri.nixosModules.niri
+    {
+      environment.systemPackages = [
+        nix-alien.packages.x86_64-linux.nix-alien
+      ];
+      nixpkgs.overlays = [
+        copyparty.overlays.default
+        niri.overlays.niri
+      ];
+      programs.nix-ld.enable = true;
+    }
   ];
 }
